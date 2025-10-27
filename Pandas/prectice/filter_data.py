8f9d5ee295ab85c3aabdc0755ee5c53c7c9e7fe5
Dharm2005 <dharmdobariya7016@gmail.com>
2025-10-27 10:08:53 +0530

    filter usibng str and query funcs

--- Pandas/prectice/filter_data.py
+++ Pandas/prectice/filter_data.py
@@ -15,4 +15,17 @@ print(bios[bios["height_cm"] > 220]["name"]) # without using loc , can only get 
 print(bios.loc[(bios["height_cm"] > 220) & (bios["weight_kg"] > 135), ["name","height_cm","weight_kg"]]) # use format like () & ()
 
 
-# STRING METHOD
+# STRING METHOD TO FILTER DATA
+
+# .contains()
+print(bios[bios['name'].str.contains("patel",case=False)])
+
+# .isin()
+print(bios[bios["born_country"].isin(["UAS","FRA"])])
+
+# .startwith()
+print(bios[bios["name"].str.startswith("Dharm")])
+
+# .query function
+
+print(bios.query("born_country == 'IND' and born_city == 'Ahmedabad'"))
